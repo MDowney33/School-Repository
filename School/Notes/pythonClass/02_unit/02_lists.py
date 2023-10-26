6c12d593a9272af95faad4a7a28c02a43e7d0a1c
MDowney33 <144389218+MDowney33@users.noreply.github.com>
2023-10-26 17:39:19 +0000

    Lists

---
+++ School/Notes/pythonClass/02_unit/02_lists.py
@@ -0,0 +1,12 @@
+
+s = 'hello'
+print(s)
+s+= ' world'
+print(s)
+
+#The syntax for a list literal is to use square brackets
+
+hi = [1,2,3,4,5]
+#can mutate list
+hi[3] = 4.5
+print(int.__add__(5,10))
