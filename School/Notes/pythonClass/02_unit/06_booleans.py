12d014896e338f6ed13db34894625dba3b3bf6e6
MDowney33 <144389218+MDowney33@users.noreply.github.com>
2023-11-09 19:16:24 +0000

    Bools

--- School/Notes/pythonClass/02_unit/06_booleans.py
+++ School/Notes/pythonClass/02_unit/06_booleans.py
@@ -1,4 +1,14 @@
-
 # boolean is true or false
 # boolean class is technically subclass of int
 # and, or, not
+
+# if
+# else
+# elif
+
+# operators >,<,<=,>=,==,!=,in
+# installing unity without admin
+
+import myStuff
+if "joe" in myStuff.myLists.myString:
+    print("yay")
