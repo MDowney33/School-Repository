74cf60ffeb0f0ca9205a39d948d1963e8d7676c7
MDowney33 <144389218+MDowney33@users.noreply.github.com>
2024-04-24 16:47:19 +0000

    khjlkjhjh

--- webApp/testProject/testProject/urls.py
+++ webApp/testProject/testProject/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
@@ -23,5 +23,5 @@ urlpatterns = [
     path('stuff/', views.helloWorld),
     path('', views.home),
     path('about/', views.about),
-    path('blog/', views.blog),
+    path('blog/', include('pages.urls')),
 ]
